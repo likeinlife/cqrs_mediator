245bec231463db1528308751e841b6db2c045541
likeinlife <likeinlife@outlook.com>
2024-07-06 23:02:21 +0300

    fix(interfaces.handlers.query): query handler typevar

--- meator/interfaces/handlers/query.py
+++ meator/interfaces/handlers/query.py
@@ -5,6 +5,7 @@ from meator.entities import Query
 from .request import IHandler
 
 HRes = tp.TypeVar("HRes")
+QType = tp.TypeVar("QType", bound=Query)
 
 
-class IQueryHandler(IHandler[Query, HRes]): ...
+class IQueryHandler(IHandler[QType, HRes]): ...
